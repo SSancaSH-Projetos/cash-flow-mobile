70b050dd41bc1eb7485c33f3676aa3b3d8fe482d
Rarepx <brunosantiago7002@gmail.com>
2024-04-15 08:43:13 -0300

    correçao do envio via camera

--- src/pages/AddExpenses/index.js
+++ src/pages/AddExpenses/index.js
@@ -9,170 +9,163 @@ import { useFocusEffect, useNavigation , useRoute} from '@react-navigation/nativ
 import { AddExpensesMethod } from '../../service/ExpensesService';
 
 const inititialExpense = {
-    description: '',
-    category: '',
-    amount: '',
-    invoice: null,
-    id_travel:'' 
+description: '',
+category: '',
+amount: '',
+invoice: null,
+id_travel:'' 
 };
 
 function ImageViewer({ placeholderImageSource, selectedImage }) {
-    const imageSource = selectedImage ? { uri: selectedImage } : placeholderImageSource;
-    return <Image source={imageSource} style={Styles.image} />;
+const imageSource = selectedImage ? { uri: selectedImage } : placeholderImageSource;
+return <Image source={imageSource} style={Styles.image} />;
 }
 
 export default function AddExpenses() {
-    const [expense, setExpense] = useState(inititialExpense);
-    const [selectedImage, setSelectedImage] = useState(null);
-    const navigation = useNavigation();
-    const PlaceholderImage = require('../../img/notaFiscal.png');
-    const [visible, setVisible] = useState(false);
-    const route = useRoute();
-    const { id } = route.params;
+const [expense, setExpense] = useState(inititialExpense);
+const [selectedImage, setSelectedImage] = useState(null);
+const navigation = useNavigation();
+const PlaceholderImage = require('../../img/notaFiscal.png');
+const [visible, setVisible] = useState(false);
+const route = useRoute();
+const { id } = route.params;
 
-    async function requestCameraPermission() {
-        await Camera.requestCameraPermissionsAsync();
-        setExpense(({ ...expense, id_travel: id })); 
+async function requestCameraPermission() {
+    await Camera.requestCameraPermissionsAsync();
+    setExpense(({ ...expense, id_travel: id })); 
+}
+
+useFocusEffect(useCallback(() => { requestCameraPermission() }, []));
+
+const pickImage = async (amount) => {
+    closeModal();
+    let result;
+    if (amount) {
+        result = await ImagePicker.launchImageLibraryAsync({
+            quality: 1,
+            allowsEditing: true,
+        });
+        if (!result.canceled) {
+            setSelectedImage(result.assets[0].uri);
+            setExpense({ ...expense, invoice: result.assets[0] }); 
+        }
+    } else {
+        result = await ImagePicker.launchCameraAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            aspect: [4, 3],
+            quality: 1,
+        });
+        if (!result.canceled) {
+            setSelectedImage(result.assets[0].uri);
+            setExpense({ ...expense, invoice: result.assets[0] }); 
+            console.log(result);
+        }
     }
+};
 
-    useFocusEffect(useCallback(() => { requestCameraPermission() }, []));
-
-    const takePicture = async () => {
-        if (Camera) {
-            const photo = await camera.takePictureAsync();
-            setExpense({ ...expense, invoice: photo.uri });
-        }
-    };
-
-    const pickImage = async (amount) => {
-        closeModal();
-        let result;
-        if (amount) {
-            result = await ImagePicker.launchImageLibraryAsync({
-                quality: 1,
-                allowsEditing: true,
-            });
-            if (!result.canceled) {
-                setSelectedImage(result.assets[0].uri);
-                setExpense({ ...expense, invoice: result.assets[0] }); 
-            }
-        } else {
-            result = await ImagePicker.launchCameraAsync({
-                mediaTypes: ImagePicker.MediaTypeOptions.Images,
-                aspect: [4, 3],
-                quality: 1,
-                allowsEditing: true,
-            });
-            if (!result.canceled) {
-                setSelectedImage(result.uri || (result.assets.length > 0 && result.assets[0].uri));
-                setExpense({ ...expense, invoice: result || (result.assets.length > 0 && result.assets[0]) });
-            }
-        }
-    };
-    
-    function addExpense() {
-        if (!validateExpenses()) {
-            //setAlertEmptyInput('Todos os campos devem ser preenchidos');
-        }
-        //setAlertEmptyInput('');
-        console.log("expenses:"+ expense)
-        const success = AddExpensesMethod(expense);
-        if (!success) {
-            //setAlertEmptyInput('Erro ao adicionar a despesa');
-        }
-        setSelectedImage(null);
-        navigation.navigate('TravelDescription', { id });
+function addExpense() {
+    if (!validateExpenses()) {
+        //setAlertEmptyInput('Todos os campos devem ser preenchidos');
     }
-    
-
-    const validateExpenses = () => {
-        const { description, category, amount, invoice } = expense;
-        console.log(expense)
-        if(
-            description.trim() !== '' &&
-            category.trim() !== '' &&
-            amount.trim() !== '' &&
-            invoice !== null 
-        ){
-            return true;
-        }else{
-            return false;
-        }
-    };
-    
-
-    function openModal() {
-        setVisible(true);
+    //setAlertEmptyInput('');
+    console.log("expenses:"+ expense)
+    const success = AddExpensesMethod(expense);
+    if (!success) {
+        //setAlertEmptyInput('Erro ao adicionar a despesa');
     }
+    setSelectedImage(null);
+    navigation.navigate('TravelDescription', { id });
+}
 
-    function closeModal() {
-        setVisible(false);
+
+const validateExpenses = () => {
+    const { description, category, amount, invoice } = expense;
+    console.log(expense)
+    if(
+        description.trim() !== '' &&
+        category.trim() !== '' &&
+        amount.trim() !== '' &&
+        invoice !== null 
+    ){
+        return true;
+    }else{
+        return false;
     }
+};
 
-    return (
-        <View style={Styles.container}>
-            <Header />
-            <View style={Styles.form}>
-                <TextInput
-                    style={Styles.input}
-                    placeholder='Descrição'
-                    onChangeText={text => setExpense({ ...expense, description: text })}
-                />
-                <TextInput
-                    style={Styles.input}
-                    placeholder='Categoria'
-                    onChangeText={text => setExpense({ ...expense, category: text })}
 
-                />
-                <TextInput
-                    style={Styles.input}
-                    placeholder='Valor'
-                    keyboardType='numeric'
-                    onChangeText={text => setExpense({ ...expense, amount: text })}
+function openModal() {
+    setVisible(true);
+}
 
-                />
+function closeModal() {
+    setVisible(false);
+}
+
+return (
+    <View style={Styles.container}>
+        <Header />
+        <View style={Styles.form}>
+            <TextInput
+                style={Styles.input}
+                placeholder='Descrição'
+                onChangeText={text => setExpense({ ...expense, description: text })}
+            />
+            <TextInput
+                style={Styles.input}
+                placeholder='Categoria'
+                onChangeText={text => setExpense({ ...expense, category: text })}
+
+            />
+            <TextInput
+                style={Styles.input}
+                placeholder='Valor'
+                keyboardType='numeric'
+                onChangeText={text => setExpense({ ...expense, amount: text })}
+
+            />
+        </View>
+
+        <TouchableOpacity style={Styles.areaButton} onPress={openModal} >
+
+            <View style={Styles.button}>
+                <Text style={Styles.textButton}>Anexar Nota Fiscal</Text>
+                <Icon name="add-a-photo" size={30} color="#000" />
             </View>
+        </TouchableOpacity>
 
-            <TouchableOpacity style={Styles.areaButton} onPress={openModal} >
+        <Modal animationType='fade' visible={visible} style={Styles.areaModal} onRequestClose={closeModal}>
 
-                <View style={Styles.button}>
-                    <Text style={Styles.textButton}>Anexar Nota Fiscal</Text>
-                    <Icon name="add-a-photo" size={30} color="#000" />
-                </View>
-            </TouchableOpacity>
-
-            <Modal animationType='fade' visible={visible} style={Styles.areaModal} onRequestClose={closeModal}>
-
-                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                    <View style={Styles.modalContainer}>
-                        <View style={Styles.buttonsContainerModal}>
-                            <TouchableOpacity style={Styles.buttonModal} onPress={() => pickImage(false)}>
-                                <Text style={Styles.buttonTextModal}>Selecionar Câmera</Text>
-                            </TouchableOpacity>
-                            <TouchableOpacity style={Styles.buttonModal} onPress={() => pickImage(true)}>
-                                <Text style={Styles.buttonTextModal}>Selecionar Arquivo</Text>
-                            </TouchableOpacity>
-                        </View>
-                        <TouchableOpacity style={Styles.buttonModalCan} onPress={closeModal}>
-                            <Text style={Styles.buttonTextModal}>Cancelar</Text>
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <View style={Styles.modalContainer}>
+                    <View style={Styles.buttonsContainerModal}>
+                        <TouchableOpacity style={Styles.buttonModal} onPress={() => pickImage(false)}>
+                            <Text style={Styles.buttonTextModal}>Selecionar Câmera</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={Styles.buttonModal} onPress={() => pickImage(true)}>
+                            <Text style={Styles.buttonTextModal}>Selecionar Arquivo</Text>
                         </TouchableOpacity>
                     </View>
+                    <TouchableOpacity style={Styles.buttonModalCan} onPress={closeModal}>
+                        <Text style={Styles.buttonTextModal}>Cancelar</Text>
+                    </TouchableOpacity>
                 </View>
-                
-            </Modal>
-
-            <View style={Styles.imageContainer}>
-                <ImageViewer
-                    placeholderImageSource={PlaceholderImage}
-                    selectedImage={selectedImage}
-                />
             </View>
+            
+        </Modal>
 
-            <TouchableOpacity style={Styles.areaButtonAdd} onPress={() => addExpense()}>
-                <View style={Styles.button}>
-                    <Text style={Styles.textButton}>ADICIONAR DESPESA</Text>
-                </View>
-            </TouchableOpacity>
+        <View style={Styles.imageContainer}>
+            <ImageViewer
+                placeholderImageSource={PlaceholderImage}
+                selectedImage={selectedImage}
+            />
         </View>
-    );
+
+        <TouchableOpacity style={Styles.areaButtonAdd} onPress={() => addExpense()}>
+            <View style={Styles.button}>
+                <Text style={Styles.textButton}>ADICIONAR DESPESA</Text>
+            </View>
+        </TouchableOpacity>
+    </View>
+);
 }
--- src/service/ExpensesService/index.js
+++ src/service/ExpensesService/index.js
@@ -40,7 +40,7 @@ export async function AddInvoiceAExpenses(invoice,url){
         uri: Platform.OS === 'ios' ? invoice.uri.replace('file://', '') : invoice.uri,
         name: 'fiscalNote.png',
         type: 'image/png', 
-    });
+    })
 
 const response = await fetch(url+"/fiscalNote", {
     method: 'PUT',
@@ -76,6 +76,24 @@ export async function FindExpensesMethod(id_travel , id_expenses) {
     } catch (error) {
         console.error('Error finding expenses:', error.message);
         return null;
+    }
+}
+
+export async function FindImageInvoiceExpenses(id_travel, id_expenses) {
+    try{
+        const response = await fetch(`${apiUrl}/api/travels/${id_travel}/expenses/${id_expenses}/fiscalNote`,{
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        if(!response.ok){
+            throw new Error("response is not ok to get a Invoice Image")
+        }
+        const foundExpenses = await response.json();
+        return foundExpenses;
+    }catch{
+        throw new Error("Failed to get a Invoice Image")
     }
 }
 
