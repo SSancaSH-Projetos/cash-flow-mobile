7b196c6b126bc2a7bb06e5a70bacf7101a95f82c
otreborBz <rlcarval38@gmail.com>
2024-05-21 08:36:29 -0300

    modificado modal para alert na saida do logout do usuario

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,30 +1,35 @@
-import React, { useState } from "react";
-import { View, Image, TouchableOpacity, Modal, Button, Text } from "react-native";
+import React from "react";
+import { View, Image, TouchableOpacity, Alert } from "react-native";
 import Icon from 'react-native-vector-icons/Entypo';
 import Styles from './Styles'
 import { useNavigation } from "@react-navigation/native";
-import styles from "./Styles";
 import { serviceLogoutMethod } from "../../service/UserService";
 
 
 export default function Header() {
     const navigation = useNavigation();
-    const [modalVisible, setModalVisible] = useState(false);
 
-    async function handleLogout(){
-        setModalVisible(true);
-    }
-
-    async function confirmLogout() {
-        await logOut();
-    }
-
-    async function logOut() {
-        const response = await serviceLogoutMethod();
-        if(response){
-            setModalVisible(false);
-            navigation.navigate('Welcome');
-        }
+    function confirmLogout() {
+        Alert.alert(
+            'Sair',
+            'Ao sair a sessão será finalizada.',
+            [
+                {
+                    text: 'Cancelar',
+                    style: 'cancel',
+                },
+                {
+                    text: 'Confirmar',
+                    onPress: async () => {
+                        const response = await serviceLogoutMethod();
+                        if(response){
+                            navigation.navigate('Welcome');
+                        }
+                    },
+                },
+            ],
+            { cancelable: false }
+        );
     }
 
     return(
@@ -34,27 +39,9 @@ export default function Header() {
                     source={require('../../img/logo_branca.png')}
                     style={{width:54, height:49}} />
             </TouchableOpacity>
-            <TouchableOpacity onPress={handleLogout}>
+            <TouchableOpacity onPress={confirmLogout}>
                  <Icon name="menu" size={50} color="#fff"/>
             </TouchableOpacity>
-
-            <Modal
-                animationType="slide"
-                transparent={true}
-                visible={modalVisible}
-                onRequestClose={() => {
-                    setModalVisible(false);
-                }}
-            >
-                <View style={Styles.modalContainer}>
-                    <View style={Styles.modalView}>
-                        <Text style={Styles.modalText}>Tem certeza que deseja Sair?</Text>
-                        <TouchableOpacity style={styles.btnLogout}onPress={confirmLogout}>
-                            <Text style={styles.buttonText}>Sair</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            </Modal>
         </View>
     );
 }
